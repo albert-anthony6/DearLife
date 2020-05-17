59df665b39d8f397e87534b09fe5acb3c7f27b15
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-16 23:49:40 -0500

    Added security npm packages to defend against malicious attacks.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +13,36 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
-app.use(express.json()); // Data from the body is added to req
+
+// Limit requests from same IP
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' })); // Data from the body is added to req
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: ['commentsQuantity', 'category', 'slug']
+}));
 
 // ROUTES
 app.use('/api/v1/posts', postRouter); // Mounting Routers
