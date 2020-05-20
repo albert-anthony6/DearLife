5bf9c5f2ea350c19e6ceedd01218c5f49dbf2604
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-20 02:10:44 -0500

    Added text to landing page

--- client/src/pages/Home.component.jsx
+++ client/src/pages/Home.component.jsx
@@ -4,6 +4,11 @@ import Footer from '../components/footer/Footer.component';
 
 const Home = () => (
     <div className="hero">
+        <div className="hero__text">
+            <h1>Find your passion.</h1>
+            <p>Join the dearlife community and share your passion with others all around the globe.</p>
+            <button>Start for free</button>
+        </div>
         <Footer/>
     </div>
 );
