a2eab56aced87bff7c7027a7384477d4635ed45b
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-20 10:05:48 -0500

    Added signup function

--- client/src/components/signin-signup/SignUp.component.jsx
+++ client/src/components/signin-signup/SignUp.component.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LogInSignUp.styles.scss';
+import axios from 'axios';
 
 const SignUp = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
+
+    const signup = async (name, email, password, passwordConfirm) => {
+        try{
+            const res = await axios({
+                method: 'POST',
+                url: '/api/v1/users/signup',
+                data: {
+                    name,
+                    email,
+                    password,
+                    passwordConfirm
+                }
+            });
+            console.log(res);
+        } catch(err) {
+            console.log(err.response.data);
+        }
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        signup(name, email, password, confirm);
+        setName('');
+        setEmail('');
+        setPassword('');
+        setConfirm('');
+    };
+
     return(
-        <form className="form form--signup">
+        <form onSubmit={handleSubmit} className="form form--signup">
             <h2 className="form__h2">Create your account!</h2>
             
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" autoComplete="off"/>
+            <input onChange={e => setName(e.target.value)} value={name} type="text" id="name" autoComplete="off"/>
 
             <label htmlFor="email">Email address</label>
-            <input type="email" id="email" autoComplete="off"/>
+            <input onChange={e => setEmail(e.target.value)} value={email} type="email" id="email" autoComplete="off"/>
             
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" autoComplete="off"/>
+            <input onChange={e => setPassword(e.target.value)} value={password} type="password" id="password" autoComplete="off"/>
 
             <label htmlFor="confirm">Confirm password</label>
-            <input type="password" id="confirm" autoComplete="off"/>
+            <input onChange={e => setConfirm(e.target.value)} value={confirm} type="password" id="confirm" autoComplete="off"/>
             
             <button className="form__btn">Sign Up</button>
         </form>
