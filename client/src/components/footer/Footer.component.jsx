466c9009776a892625284bf205d9a929163ee84f
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-20 01:10:49 -0500

    Added footer component to home page

---
+++ client/src/components/footer/Footer.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Footer.styles.scss';
+
+const Footer = () => (
+    <footer className="footer">
+        <ul className="footer__list">
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">About</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Help</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Posts</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Guidelines</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Blog</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Help forum</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Developers</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Jobs</a>
+            </li>
+            <li className="footer__item">
+                <a href="https://www.Google.com" className="footer__link">Contact</a>
+            </li>
+        </ul>
+
+        <ul className="footer__list">
+            <li>
+                <a className="footer__link footer__link--icon" href="https://github.com/albert-anthony6" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            </li>
+            <li>
+                <a className="footer__link footer__link--icon" href="https://www.linkedin.com/in/avaldes21/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+            </li>
+        </ul>
+    </footer>
+);
+
+export default Footer;
--- client/src/pages/Home.component.jsx
+++ client/src/pages/Home.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Home.styles.scss';
+import Footer from '../components/footer/Footer.component';
 
 const Home = () => (
     <div className="hero">
-        
+        <Footer/>
     </div>
 );
 
