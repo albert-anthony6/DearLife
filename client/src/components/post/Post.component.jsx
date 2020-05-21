e2562a4d487a71ca5e96b07eaca9874c4765bac8
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-21 00:39:02 -0500

    Added basic posts layout on post page

--- client/src/components/post/Post.component.jsx
+++ client/src/components/post/Post.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Post.styles.scss';
 
-const Post = () => (
+const Post = ({ post }) => (
     <div className="post">
-        {/* <img src="" alt=""/> */}
+        <img className="post__img" src={post.imageCover} alt="image"/>
+        <h3 className="post__h3">{post.title}</h3>
+        <p className="post__date">{post.createdAt}</p>
+        <p className="post__summary">{post.summary}</p>
     </div>
 );
 
--- client/src/pages/posts/Posts.component.jsx
+++ client/src/pages/posts/Posts.component.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Posts.styles.scss';
 import Post from '../../components/post/Post.component';
+import axios from 'axios';
 
 const Posts = () => {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        try{
+            const res = await axios({
+                method: 'GET',
+                url: '/api/v1/posts/'
+            });
+            // console.log(res);
+            setData(res.data.data.data);
+        } catch(err) {
+            alert("ERROR: " + err.response.data.message);
+        }
+    }
+    
+    useEffect(() => {
+        if(sessionStorage.homePosts) {
+            console.log("Getting from session storage...");
+            setData(JSON.parse(sessionStorage.homePosts));
+        } else {
+            console.log("Fetching from API...");
+            fetchData();
+        }
+    }, []);
+    
+    useEffect(() => {
+        console.log("Writing to session storage...");
+        sessionStorage.setItem('homePosts', JSON.stringify(data));
+    }, [data]);
+
     return(
         <div className="posts">
-            <Post/>
+            {data.map(el => <Post key={el._id} post={el} />)}
         </div>
     );
 };
