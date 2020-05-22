6b4501752103a329f00441ed6dd10a57b3e23b95
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-21 21:56:52 -0500

    Added conditional rendering to Alerts component with custom css modifiers and animation for login page

--- client/src/components/alerts/Alerts.component.jsx
+++ client/src/components/alerts/Alerts.component.jsx
@@ -4,7 +4,9 @@ import './Alerts.styles.scss';
 import { connect } from 'react-redux';
 
 const Alerts = ({ status }) => (
-    <div className="alerts">
+    // Nested ternary operator would also work
+    // <div className={`alerts ${status === 'success' ? 'alerts--ok' : (!status ? null : 'alerts--bad') }`}>
+    <div className={`alerts ${status === 'success' ? 'alerts--ok' : 'alerts--bad'}`}>
         <p className="alerts__text">{status}</p>
     </div>
 );
--- client/src/pages/login/LogIn.component.jsx
+++ client/src/pages/login/LogIn.component.jsx
@@ -4,12 +4,31 @@ import SignIn from '../../components/signin-signup/SignIn.component';
 import SignUp from '../../components/signin-signup/SignUp.component';
 import Alerts from '../../components/alerts/Alerts.component';
 
-const LogIn = ({}) => (
-    <div className="login-signup">
-        <Alerts/>
-        <SignIn/>
-        <SignUp/>
-    </div>
-);
+import { connect } from 'react-redux';
+import { setUserStatus } from '../../redux/user/user.actions';
 
-export default LogIn;
+const LogIn = ({ status, setUserStatus }) => {
+    if(status){
+        setTimeout(() => {
+            setUserStatus('');
+        }, 2000);
+    }
+
+    return(
+        <div className="login-signup">
+            {!status ? null : <Alerts/>}
+            <SignIn/>
+            <SignUp/>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    status: state.user.status
+});
+
+const mapDispatchToProps = dispatch => ({
+    setUserStatus: status => dispatch(setUserStatus(status))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn);
